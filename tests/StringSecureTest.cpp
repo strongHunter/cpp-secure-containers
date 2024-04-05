86720e83f9b78efec38a9d81f8e5e68044803c09
strongHunter <@>
2024-04-05 23:09:48 +0300

    FIX. Исправлено название теста

--- tests/StringSecureTest.cpp
+++ tests/StringSecureTest.cpp
@@ -41,7 +41,7 @@ TEST(StringSecureTest, DefaultAllocatorShouldBe_SanitizingAllocator)
     EXPECT_TRUE((std::is_same<typename decltype(str)::allocator_type, SanitizingAllocator<char>>::value));
 }
 
-TEST(StringSecureTest, CanDefineVectorWithDerivedSanitizingAllocator)
+TEST(StringSecureTest, CanDefineStringWithDerivedSanitizingAllocator)
 {
     basic_string_secure<char, SanitizingAllocatorChild<char>> str;
     EXPECT_TRUE((std::is_base_of<SanitizingAllocator<char>, decltype(str)::allocator_type>::value));
