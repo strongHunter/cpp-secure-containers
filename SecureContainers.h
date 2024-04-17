0bd1a9fb4da61805d83187e625cdcfd1866833d8
strongHunter <@>
2024-04-17 22:14:25 +0300

    Раскомментирован "TEMP COMMENTED" код

--- SecureContainers.h
+++ SecureContainers.h
@@ -224,14 +224,13 @@ using u16string_secure = basic_string_secure<char16_t>;
 using u32string_secure = basic_string_secure<char32_t>;
 
 
-// TEMP COMMENTED
-//template<typename CharT, IsSanitizingAllocator Alloc, typename RhsT>
-//basic_string_secure<CharT, Alloc> operator+(const basic_string_secure<CharT, Alloc>& lhs, RhsT&& rhs)
-//{
-//    basic_string_secure<CharT, Alloc> result(basic_string_secure<CharT, Alloc>::copy(lhs));
-//    result += std::forward<RhsT>(rhs);
-//    return result;
-//}
+template<typename CharT, IsSanitizingAllocator Alloc, typename RhsT>
+basic_string_secure<CharT, Alloc> operator+(const basic_string_secure<CharT, Alloc>& lhs, RhsT&& rhs)
+{
+    basic_string_secure<CharT, Alloc> result(basic_string_secure<CharT, Alloc>::copy(lhs));
+    result += std::forward<RhsT>(rhs);
+    return result;
+}
 
 template<typename CharT, IsSanitizingAllocator Alloc, typename RhsT>
 basic_string_secure<CharT, Alloc> operator+(basic_string_secure<CharT, Alloc>&& lhs, RhsT&& rhs)
--- tests/StringSecureConcatenationTest.cpp
+++ tests/StringSecureConcatenationTest.cpp
@@ -5,39 +5,39 @@
 
 using testing::_;
 
-// TEMP COMMENTED
-//TEST(StringSecureConcatenationTest, StringSecure_Plus_ConstChar)
-//{
-//    string_secure s1 = "123";
-//    string_secure s = s1 + "456";
-//
-//    EXPECT_EQ(s, "123456");
-//}
-//
-//TEST(StringSecureConcatenationTest, StringSecure_Plus_Char)
-//{
-//    string_secure s1 = "123";
-//    string_secure s = s1 + '4';
-//
-//    EXPECT_EQ(s, "1234");
-//}
-//
-//TEST(StringSecureConcatenationTest, StringSecure_Plus_lvalue_StringSecure)
-//{
-//    string_secure s1 = "123";
-//    string_secure s2 = "456";
-//    string_secure s = s1 + s2;
-//
-//    EXPECT_EQ(s, "123456");
-//}
-//
-//TEST(StringSecureConcatenationTest, StringSecure_Plus_rvalue_StringSecure)
-//{
-//    string_secure s1 = "123";
-//    string_secure s = s1 + string_secure("456");
-//
-//    EXPECT_EQ(s, "123456");
-//}
+
+TEST(StringSecureConcatenationTest, StringSecure_Plus_ConstChar)
+{
+    string_secure s1 = "123";
+    string_secure s = s1 + "456";
+
+    EXPECT_EQ(s, "123456");
+}
+
+TEST(StringSecureConcatenationTest, StringSecure_Plus_Char)
+{
+    string_secure s1 = "123";
+    string_secure s = s1 + '4';
+
+    EXPECT_EQ(s, "1234");
+}
+
+TEST(StringSecureConcatenationTest, StringSecure_Plus_lvalue_StringSecure)
+{
+    string_secure s1 = "123";
+    string_secure s2 = "456";
+    string_secure s = s1 + s2;
+
+    EXPECT_EQ(s, "123456");
+}
+
+TEST(StringSecureConcatenationTest, StringSecure_Plus_rvalue_StringSecure)
+{
+    string_secure s1 = "123";
+    string_secure s = s1 + string_secure("456");
+
+    EXPECT_EQ(s, "123456");
+}
 
 TEST(StringSecureConcatenationTest, rvalue_StringSecure_Plus_lvalue_StringSecure)
 {
