fb9b605eb69cd951f2ba7a6a4c021c3428ae5310
strongHunter <@>
2024-04-17 20:40:02 +0300

    Добавлен файл для тестов сложения string_secure

---
+++ tests/StringSecureConcatenationTest.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "SecureContainers.h"
+
+using testing::_;
+
+TEST(StringSecureConcatenationTest, StringSecure_Plus_ConstChar)
+{
+    string_secure s1 = "123";
+    string_secure s = s1 + "456";
+}
