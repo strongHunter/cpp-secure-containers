0fc6dd697df9638f6c2fc86ab9423b7e73ab1709
strongHunter <@>
2024-04-18 23:05:39 +0300

    Реализация basic_string_secure::substr

--- SecureContainers.h
+++ SecureContainers.h
@@ -184,6 +184,12 @@ public:
         }
     }
 
+    // TODO: test
+    constexpr basic_string_secure substr(size_type pos = 0, size_type count = basic_string_secure::npos) const
+    {
+        return std::basic_string<T, std::char_traits<T>, Alloc>::substr(pos, count);
+    }
+
     template<typename LhsT, typename RhsT>
     friend basic_string_secure operator+(LhsT&& lhs, RhsT&& rhs)
     {
